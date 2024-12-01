76e62552074ab0873959282220c18f1d1bae6ee3
111189198131 <92156429+111189198131@users.noreply.github.com>
2024-12-01 11:57:35 +0000

    DataTypes

---
+++ JSBasics/02_DataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+
+let age = 12;
+let name = null;
+console.log(typeof null);
+//number => 2 to power  53 
+// bigInt 
+// String => ""
+//boolean => true /false 
+// null => standalone  value => let state = null
+//undefined => Not assign any value
+//symbol => unique 
